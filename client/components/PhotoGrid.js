43ff359653b5e1566f9afb26ec87dcf64f9ddee9
Rafa Ramirez <raffe90@gmail.com>
2016-07-08 00:36:16 +0200

    Adds PhotoGrid & Single components. Basic react-router implementation

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+class PhotoGrid extends React.Component{
+  constructor(){
+    super()
+  }
+
+  render(){
+    return (
+      <div>
+        <h2>
+          I'm the PhotoGrid
+        </h2>
+      </div>
+    )
+  }
+}
+
+export default PhotoGrid
---
+++ client/components/Single.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+class Single extends React.Component{
+  constructor(){
+    super()
+  }
+
+  render(){
+    return (
+      <div>
+        <h2>
+          I'm the single
+        </h2>
+      </div>
+    )
+  }
+}
+
+export default Single
