7919c9a4b07ac4cf3f3f15b32179f66a834ab87f
Rafa Ramirez <raffe90@gmail.com>
2016-07-08 00:24:43 +0200

    Initial commit with basic config & Main component

---
+++ client/components/Main.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+class Main extends React.Component{
+	constructor(){
+		super()
+	}
+
+	render(){
+		return (
+			<div>
+				<h1>
+					<Link to="/">Reduxstagram</Link>
+				</h1>
+			</div>
+		)
+	}
+}
+
+export default Main
